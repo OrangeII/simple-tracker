42038db363fef227348d88368ce3866154b1826d
OrangeII <ivaldinico@gmail.com>
2025-02-09 12:32:26 +0100

    start pwa dev

--- frontend/simple-tracker/vite.config.ts
+++ frontend/simple-tracker/vite.config.ts
@@ -2,8 +2,35 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import mkcert from "vite-plugin-mkcert";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), mkcert(), tailwindcss()],
+  plugins: [
+    vue(),
+    mkcert(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "simple time tracker",
+        short_name: "stt",
+        description: "a simple time tracker",
+        theme_color: "#0E1E2E",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
