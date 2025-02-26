5df27ee86155d02cb45fe96100e5498bde412ece
OrangeII <ivaldinico@gmail.com>
2025-02-26 17:55:45 +0100

    add loading indication on login

--- frontend/simple-tracker/src/components/Spinner.vue
+++ frontend/simple-tracker/src/components/Spinner.vue
@@ -1,5 +1,5 @@
 <template>
   <div
-    class="border-4 border-primary border-solid border-t-transparent rounded-full animate-spin"
+    class="border-4 border-solid border-t-transparent rounded-full animate-spin"
   ></div>
 </template>
