313e8a5ff15578913044733efb5cb45039d1a6ec
OrangeII <ivaldinico@gmail.com>
2025-03-02 14:20:04 +0100

    improve entry detail page style

--- frontend/simple-tracker/src/components/AppPageEntryDetail.vue
+++ frontend/simple-tracker/src/components/AppPageEntryDetail.vue
@@ -1,24 +1,54 @@
 <template>
   <AppPage :title="toDurationString(duration)">
-    <div>
-      <h3 class="text-text/70">Name</h3>
+    <div class="pb-4">
       <h2>{{ entry.tasks?.name }}</h2>
     </div>
-    <div>
-      <table class="table-auto">
-        <tbody>
-          <tr>
-            <td>Start</td>
-            <td>{{ start.toLocaleTimeString() }}</td>
-            <td>{{ start.toLocaleDateString() }}</td>
-          </tr>
-          <tr>
-            <td>Stop</td>
-            <td>{{ end?.toLocaleTimeString() }}</td>
-            <td>{{ end?.toLocaleDateString() }}</td>
-          </tr>
-        </tbody>
-      </table>
+    <div class="pb-4 flex flex-col gap-2">
+      <div>
+        <div class="flex gap-1 items-center">
+          <PlayCircleIcon class="text-text/70 size-5"></PlayCircleIcon>
+          <h3 class="text-text/70">Start</h3>
+        </div>
+
+        <div class="flex justify-between items-center gap-4">
+          <div
+            class="p-2 rounded-md grainy bg-background dark:bg-blend-overlay grow"
+          >
+            <h2>
+              {{ start.toLocaleTimeString() }}
+            </h2>
+          </div>
+          <div
+            class="p-2 rounded-md grainy bg-background dark:bg-blend-overlay grow"
+          >
+            <h2>
+              {{ start.toLocaleDateString() }}
+            </h2>
+          </div>
+        </div>
+      </div>
+      <div v-if="stop !== null">
+        <div class="flex gap-1 items-center">
+          <StopCircleIcon class="text-text/70 size-5"></StopCircleIcon>
+          <h3 class="text-text/70">Stop</h3>
+        </div>
+        <div class="flex justify-between items-center gap-4">
+          <div
+            class="p-2 rounded-md grainy bg-background dark:bg-blend-overlay grow"
+          >
+            <h2>
+              {{ stop?.toLocaleTimeString() }}
+            </h2>
+          </div>
+          <div
+            class="p-2 rounded-md grainy bg-background dark:bg-blend-overlay grow"
+          >
+            <h2>
+              {{ stop?.toLocaleDateString() }}
+            </h2>
+          </div>
+        </div>
+      </div>
     </div>
 
     <div>
@@ -33,6 +63,7 @@ import { type TimeEntry } from "../common/types";
 import AppPage from "./AppPage.vue";
 import { toDurationString } from "../common/timeUtils";
 import { computed } from "vue";
+import { PlayCircleIcon, StopCircleIcon } from "@heroicons/vue/24/solid";
 
 const props = defineProps<{ entry: TimeEntry }>();
 
@@ -48,7 +79,7 @@ const start = computed(() => {
   return new Date(props.entry.start_time);
 });
 
-const end = computed(() => {
+const stop = computed(() => {
   if (!props.entry.end_time) return null;
   return new Date(props.entry.end_time);
 });
