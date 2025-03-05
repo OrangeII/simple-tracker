c00c194bffab5b80806770d0d3b5799f16154f91
OrangeII <ivaldinico@gmail.com>
2025-03-05 18:15:12 +0100

    add favorites removal

--- frontend/simple-tracker/src/components/FavoriteTasksList.vue
+++ frontend/simple-tracker/src/components/FavoriteTasksList.vue
@@ -1,12 +1,35 @@
 <template>
   <div class="pt-4 px-4 font-bold uppercase">favorites</div>
   <div v-for="task in favoriteTasksStore.favorites" :key="task.id">
-    {{ task.name }}
+    <EntriesListItemLayout>
+      <template #left>
+        <h3 class="truncate">{{ task.name }}</h3>
+      </template>
+      <template #actions>
+        <div class="flex items-center pr-1">
+          <div
+            class="h-full flex flex-col items-center"
+            @click="onFavorite(task)"
+          >
+            <StarIcon class="size-8 text-primary"></StarIcon>
+            <h4>Remove</h4>
+          </div>
+        </div>
+      </template>
+    </EntriesListItemLayout>
   </div>
 </template>
 
 <script setup lang="ts">
+import type { Task } from "../common/types";
 import { useFavoriteTasksStore } from "../stores/favoriteTasks";
+import EntriesListItemLayout from "./EntriesListItemLayout.vue";
+import { StarIcon } from "@heroicons/vue/24/outline";
 
 const favoriteTasksStore = useFavoriteTasksStore();
+
+const onFavorite = async (task: Task) => {
+  //remove task from favorites
+  favoriteTasksStore.removeFavorite(task);
+};
 </script>
