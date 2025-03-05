ef2d1ceb0c24a7cd15a987717f4715e8262fa965
OrangeII <ivaldinico@gmail.com>
2025-03-05 15:34:42 +0100

    add theme toggle to settings page

--- frontend/simple-tracker/src/components/AppPageSettings.vue
+++ frontend/simple-tracker/src/components/AppPageSettings.vue
@@ -1,9 +1,27 @@
 <template>
   <AppPage title="Settings" @close="emit('close')">
     <template #main>
-      <div class="flex gap-2 items-center" @click="signOut">
-        <ArrowLeftStartOnRectangleIcon class="text-primary size-8" />
-        <h3>Sign out</h3>
+      <div class="flex flex-col gap-4">
+        <div
+          class="flex gap-4 items-center"
+          @click="preferencesStore.toggleDarkMode"
+        >
+          <div>
+            <MoonIcon
+              v-if="preferencesStore.darkMode"
+              class="text-primary size-8"
+            />
+            <SunIcon v-else class="text-primary size-8" />
+          </div>
+          <h3>
+            {{ preferencesStore.darkMode ? "Dark theme" : "Light theme" }}
+          </h3>
+        </div>
+
+        <div class="flex gap-4 items-center" @click="signOut">
+          <ArrowLeftStartOnRectangleIcon class="text-primary size-8" />
+          <h3>Sign out</h3>
+        </div>
       </div>
     </template>
   </AppPage>
@@ -19,11 +37,13 @@ import {
   SunIcon,
 } from "@heroicons/vue/24/solid";
 import { useUserStore } from "../stores/user";
+import { usePreferencesStore } from "../stores/preferences";
 
 const emit = defineEmits<{
   close: [];
 }>();
 const userStore = useUserStore();
+const preferencesStore = usePreferencesStore();
 
 const signOut = async () => {
   await supabase.auth.signOut();
