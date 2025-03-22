15442a0b963f7f417b21729e00e273fe36a06eb6
OrangeII <ivaldinico@gmail.com>
2025-03-22 13:00:23 +0100

    add skeleton loader to task stats

--- frontend/simple-tracker/src/components/TaskStats.vue
+++ frontend/simple-tracker/src/components/TaskStats.vue
@@ -1,16 +1,30 @@
 <template>
-  <div v-if="stats">
+  <!-- skeleton loader -->
+  <div v-if="loading" class="animate-pulse">
+    <div class="flex flex-col gap-2">
+      <div
+        class="bg-background dark:bg-blend-overlay grainy rounded-sm size-6 w-22"
+      ></div>
+      <div
+        class="bg-background dark:bg-blend-overlay grainy rounded-sm size-6 w-24"
+      ></div>
+      <div
+        class="bg-background dark:bg-blend-overlay grainy rounded-sm size-6 w-18"
+      ></div>
+    </div>
+  </div>
+  <div v-else>
     <!-- total time -->
     <div class="flex gap-2 items-center">
       <ClockIcon class="size-4 text-text/70"></ClockIcon>
-      <h3>{{ stats.total_time.split(".")[0] }}</h3>
+      <h3>{{ stats ? stats.total_time.split(".")[0] : "00:00:00" }}</h3>
       <h4 class="text-text/70">Total time</h4>
     </div>
 
     <!-- total entries -->
     <div class="flex gap-2 items-center">
       <Bars3Icon class="size-4 text-text/70"></Bars3Icon>
-      <h3>{{ stats.entries_count }}</h3>
+      <h3>{{ stats ? stats.entries_count : "0" }}</h3>
       <h4 class="text-text/70">Entries</h4>
     </div>
   </div>
